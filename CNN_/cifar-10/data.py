1aef753f13b26eb20751f272737756ef4e2a0279
falling <zhaowenrui_8866@163.com>
2023-07-27 08:37:44 +0800

    Rebuild a part of this project (a new resnet to classify fashion mnist), change data.py

--- CNN_/cifar-10/data.py
+++ CNN_/cifar-10/data.py
@@ -11,12 +11,18 @@ DATA_PATH = r"D:\Projects\PycharmProjects\Deep-learning\data"
 
 
 def loader(train, batch_size=1):
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomCrop(32, 4)
-    ])
+    if train:
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(32, 4)
+        ])
+    else:
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
     data_set = torchvision.datasets.CIFAR10(root=DATA_PATH, train=train, download=True, transform=transform)
     set_loader = torch.utils.data.DataLoader(data_set, batch_size=batch_size, shuffle=True, num_workers=2)
     return set_loader
--- CNN_/fashion_minst/data.py
+++ CNN_/fashion_minst/data.py
@@ -10,8 +10,12 @@ from torchvision import transforms
 DATA_PATH = r"D:\Projects\PycharmProjects\Deep-learning\data"
 
 
-def loader(train, batch_size=1):
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(0.5, 0.5)])
+def loader(train, batch_size=1, resize=False):
+    if not resize:
+        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(0.5, 0.5)])
+    else:
+        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(0.5, 0.5),
+                                        transforms.Resize(224, interpolation=transforms.InterpolationMode.NEAREST)])
     data_set = torchvision.datasets.FashionMNIST(root=DATA_PATH, train=train, download=True, transform=transform)
     set_loader = torch.utils.data.DataLoader(data_set, batch_size=batch_size, shuffle=True, num_workers=2)
     return set_loader
