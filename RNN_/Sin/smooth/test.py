ee41a10f3d6dbcefcc220bf910e707c20c041774
falling <zhaowenrui_8866@163.com>
2023-08-06 17:47:56 +0800

    Rebuild Sin RNN(smooth)

---
+++ RNN_/Sin/smooth/test.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/8/6 15:17
+# @Author  : 之落花--falling_flowers
+# @File    : test.py
+# @Software: PyCharm
+import random
+
+import matplotlib.pyplot as plt
+import torch
+
+from net import Net
+
+PATH = "D:\\Projects\\PycharmProjects\\Deep-learning\\pth\\RNN\\Sin\\1\\1.pth"
+
+
+def test(length):
+    net = Net()
+    net.train(False)
+    try:
+        net.load_state_dict(torch.load(PATH, map_location=torch.device('cpu')))
+    except FileNotFoundError as e:
+        print(e)
+        exit(-1)
+    start = random.randint(0, 3)
+    x = torch.linspace(start, start + length, length * 5)
+    data = torch.sin(torch.linspace(start, start + length, length * 5))
+    out_puts = [data[0]]
+    state = torch.zeros(1, 1, 32)
+    for y in data[:length]:
+        _, state = net(out_puts[-1].reshape(1, 1, 1), state)
+        out_puts.append(y)
+    for _ in range(length * 4):
+        y, state = net(out_puts[-1].reshape(1, 1, 1), state)
+        out_puts.append(y)
+    out_puts.pop(0)
+    plt.plot(x, data)
+    plt.plot(x, torch.tensor(out_puts))
+    plt.scatter(x, data, s=5)
+    plt.scatter(x, torch.tensor(out_puts), s=5)
+    plt.show()
+
+
+def main():
+    test(50)
+
+
+if __name__ == '__main__':
+    main()
---
+++ RNN_/Sin/smooth/train.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/8/6 15:17
+# @Author  : 之落花--falling_flowers
+# @File    : train.py
+# @Software: PyCharm
+import random
+import sys
+
+import torch
+from rich.progress import track
+from torch import nn
+
+sys.path.append('../../..')
+
+import base
+from net import Net
+
+PATH = "D:\\Projects\\PycharmProjects\\Deep-learning\\pth\\RNN\\Sin\\1\\1.pth"
+EPOCH = 2000
+
+num_steps = 49
+
+
+def train(epo=1, load=True, save=False, save_path=PATH):
+    net: nn.Module = Net()
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    criterion = nn.MSELoss()
+    if load:
+        try:
+            net.load_state_dict(torch.load(PATH, map_location=torch.device('cpu')))
+        except FileNotFoundError as e:
+            print(e)
+            exit(-1)
+    begin_state = torch.zeros(1, 1, 32).detach_()
+    for epoch in track(range(epo), description="Training..."):
+        start = random.randint(0, 3)
+        data = torch.sin(torch.linspace(start, start + 10, 50))
+        x = data[:-1].reshape(1, 49, 1)
+        t = data[1:].reshape(1, 49, 1)
+        y, _ = net(x, begin_state)
+        loss = criterion(y, t)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        print(f'epoch: {epoch + 1}\tloss: {loss}')
+    if save:
+        torch.save(net.state_dict(), save_path)
+
+
+@base.ringer
+@base.timer
+def main():
+    train(EPOCH, True, True)
+
+
+if __name__ == '__main__':
+    main()
