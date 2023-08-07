7d2ba22097f62f1e6713b5fa2343c81c7ae401ef
falling <zhaowenrui_8866@163.com>
2023-08-07 18:04:59 +0800

    Add The Time Machine GRU

---
+++ RNN/The Time Machine/gru/test.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/8/2 18:55
+# @Author  : 之落花--falling_flowers
+# @File    : test.py
+# @Software: PyCharm
+import sys
+
+import torch
+
+from net import Net
+
+sys.path.append('..')
+
+import data
+
+PATH = "D:\\Projects\\PycharmProjects\\Deep-learning\\pth\\RNN\\The Time Machine\\GRU\\1.pth"
+NUM_PREDS = 50
+
+
+def test(prefix):
+    net = Net()
+    net.train(False)
+    try:
+        net.load_state_dict(torch.load(PATH, map_location=torch.device('cpu')))
+    except FileNotFoundError as e:
+        print(e)
+        exit(-1)
+    state = Net.begin_state()
+    vocab = data.Vocab(data.tokenize(data.read_time_machine(), 'char'))
+    outputs = [vocab[prefix[0]]]
+    get_input = lambda: torch.tensor(outputs[-1]).reshape(1, 1)
+    for y in prefix[1:]:
+        _, state = net(get_input(), state)
+        outputs.append(vocab[y])
+    for _ in range(NUM_PREDS):
+        y, state = net(get_input(), state)
+        outputs.append(int(y.argmax()))
+    print(''.join([vocab.idx_to_token[i] for i in outputs]))
+
+
+def main():
+    test('time traveller')
+
+
+if __name__ == '__main__':
+    main()
---
+++ RNN/The Time Machine/gru/train.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/8/2 18:54
+# @Author  : 之落花--falling_flowers
+# @File    : train.py
+# @Software: PyCharm
+import sys
+
+import torch
+from rich.progress import track
+from torch import nn
+
+from net import Net
+
+sys.path.append('..')
+sys.path.append('../../..')
+
+import data
+import base
+
+PATH = "D:\\Projects\\PycharmProjects\\Deep-learning\\pth\\RNN\\The Time Machine\\GRU\\1.pth"
+EPOCH = 1
+
+batch_size, num_steps = 32, 35
+
+
+def train(epo=1, load=True, save=False, save_path=PATH):
+    net: nn.Module = Net()
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.05, momentum=0.9)
+    criterion = nn.CrossEntropyLoss()
+    if load:
+        try:
+            net.load_state_dict(torch.load(PATH, map_location=torch.device('cpu')))
+        except FileNotFoundError as e:
+            print(e)
+            exit(-1)
+    train_iter, vocab = data.load_data_time_machine(batch_size, num_steps)
+    for epoch in track(range(epo), description='Training...'):
+        epo_loss = 0
+        for x, t in train_iter:
+            state = Net.begin_state(batch_size)
+            state.detach_()
+            y, state = net(x, state)
+            optimizer.zero_grad()
+            loss = criterion(y.reshape(-1, y.shape[-1]), t.reshape(-1))
+            epo_loss += loss
+            loss.backward()
+            optimizer.step()
+        print(f'epoch: {epoch + 1}\tloss: {epo_loss / 8}')
+    if save:
+        torch.save(net.state_dict(), save_path)
+
+
+@base.ringer
+@base.timer
+def main():
+    train(EPOCH, True)
+
+
+if __name__ == '__main__':
+    main()
