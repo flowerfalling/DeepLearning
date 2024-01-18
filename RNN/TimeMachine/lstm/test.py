e1ac43e943d38558dcff5f7c824268566305535d
flowerfalling <zhaowenrui_8866@163.com>
2024-01-18 21:42:58 +0800

    Change RNN/TimeMachine/lstm

--- RNN/TimeMachine/lstm/test.py
+++ RNN/TimeMachine/lstm/test.py
@@ -10,7 +10,7 @@ from RNN.TimeMachine import data
 from net import Net
 
 NUM_PREDS = 50
-PATH = "D:\\Projects\\PycharmProjects\\Deep-learning\\pth\\RNN\\TimeMachine\\lstm\\1.pth"
+PATH = r"D:\Projects\PycharmProjects\DeepLearning\pth\RNN\TimeMachine\lstm\1.pth"
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 net = Net().to(device)
